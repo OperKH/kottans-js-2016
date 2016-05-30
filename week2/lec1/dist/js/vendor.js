b90b402644ba5efd0c858a4232c345816c588142
OperKH <oper.kh@gmail.com>
2016-05-30 22:41:10 +0300

    Change default payment method to Credit Card.

--- week2/lec1/dist/js/vendor.js
+++ week2/lec1/dist/js/vendor.js
@@ -1,29 +1,28 @@
-webpackJsonp([1],{
-
-/***/ 0:
+webpackJsonp([1],[
+/* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(14);
+	module.exports = __webpack_require__(4);
 
 
 /***/ },
-
-/***/ 14:
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	__webpack_require__(15);
+	__webpack_require__(5);
 
 	__webpack_require__(2);
 
 /***/ },
-
-/***/ 15:
+/* 5 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
 
 /***/ }
-
-});
+]);
