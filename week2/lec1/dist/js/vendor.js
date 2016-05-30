5f07dd2bd26948800f6e9480bcefe73b2105ccc5
OperKH <oper.kh@gmail.com>
2016-05-30 22:18:55 +0300

    Add dist

---
+++ week2/lec1/dist/js/vendor.js
@@ -0,0 +1,29 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(14);
+
+
+/***/ },
+
+/***/ 14:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	__webpack_require__(15);
+
+	__webpack_require__(2);
+
+/***/ },
+
+/***/ 15:
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+
+});
